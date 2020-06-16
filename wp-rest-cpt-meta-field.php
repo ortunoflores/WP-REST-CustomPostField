7909dd5b809e7def798d5ec0d96c8635fd902fe2
Your Name <ortunoflores@gmail.com>
2020-06-16 01:10:29 -0400

    Readme updated, , comments and formating added to the plugin

--- wp-rest-cpt-meta-field.php
+++ wp-rest-cpt-meta-field.php
@@ -5,7 +5,7 @@
  * Description:         Add custom post types with meta field to Wordpress REST API
  * Author:              Fernando Ortuno
  * Version:             1.0
- * GitHub Plugin URI:   https://github.com/
+ * GitHub Plugin URI:   https://github.com/ortunoflores/WP-REST-CustomPostField
  * Text Domain:         wp-rest-cpf-meta-field
  * Domain Path:         /languages
  */
@@ -14,8 +14,8 @@ defined('ABSPATH') || exit;
 
 class WP_REST_CPF_META_FIELD {
     
-
-    public static $cpf_slug = 'my-cpf';
+    // Change CPF and the other variables here without space)
+    public static $cpf_slug = 'my-cpf';    
     public static $cpf_name = 'MY CPF';
     public static $cpf_meta = 'my-cpf-meta'; 
     public static $cpf_meta_label = 'My test custom meta';
@@ -26,40 +26,49 @@ class WP_REST_CPF_META_FIELD {
         add_action( 'rest_api_init', [__CLASS__, 'add_cpf_data'] );
     }    
 
-
+    /**
+     * This method register book post type
+     *
+     * @return void
+     */
     public static function register_post_types(){
-	    register_post_type( self::$cpf_slug, [
-		    'label'  => null,
-		    'labels' => [
-			    'name'               => __(self::$cpf_slug, 'wp-rest-cpf-meta-field'),
-			    'singular_name'      => __('cpf', 'wp-rest-cpf-meta-field'),
-			    'add_new'            => __('add new cpf', 'wp-rest-cpf-meta-field'),
-			    'add_new_item'       => __('add new cpf', 'wp-rest-cpf-meta-field'),
-			    'edit_item'          => __('edit cpf', 'wp-rest-cpf-meta-field'),
-			    'new_item'           => __('new cpf', 'wp-rest-cpf-meta-field'),
-			    'view_item'          => __('view cpf', 'wp-rest-cpf-meta-field'),
-			    'search_items'       => __('search cpf', 'wp-rest-cpf-meta-field'),
-			    'not_found'          => __('not found cpf', 'wp-rest-cpf-meta-field'),
-			    'not_found_in_trash' => __('not found cpf in trash', 'wp-rest-cpf-meta-field'),
-			    'menu_name'          => __(self::$cpf_name, 'wp-rest-cpf-meta-field'),
-		    ],
-		    'description'           => __('cpf', 'wp-rest-cpf-meta-field'),
-		    'public'                => true,
-		    'show_in_menu'          => true,
+        register_post_type( self::$cpf_slug, [
+            'label'  => null,
+            'labels' => [
+                'name'               => __(self::$cpf_slug, 'wp-rest-cpf-meta-field'),
+                'singular_name'      => __('cpf', 'wp-rest-cpf-meta-field'),
+                'add_new'            => __('add new cpf', 'wp-rest-cpf-meta-field'),
+                'add_new_item'       => __('add new cpf', 'wp-rest-cpf-meta-field'),
+                'edit_item'          => __('edit cpf', 'wp-rest-cpf-meta-field'),
+                'new_item'           => __('new cpf', 'wp-rest-cpf-meta-field'),
+                'view_item'          => __('view cpf', 'wp-rest-cpf-meta-field'),
+                'search_items'       => __('search cpf', 'wp-rest-cpf-meta-field'),
+                'not_found'          => __('not found cpf', 'wp-rest-cpf-meta-field'),
+                'not_found_in_trash' => __('not found cpf in trash', 'wp-rest-cpf-meta-field'),
+                'menu_name'          => __(self::$cpf_name, 'wp-rest-cpf-meta-field'),
+            ],
+            'description'           => __('cpf', 'wp-rest-cpf-meta-field'),
+            'public'                => true,
+            'show_in_menu'          => true,
             'show_in_rest'          => true,
             "rest_base"             => "",
             "rest_controller_class" => "WP_REST_Posts_Controller",
-		    'menu_icon'             => 'dashicons-book-alt',
-		    'hierarchical'          => false,
-		    'supports'              => ['title', 'editor', 'custom-fields'],
-		    'taxonomies'            => [],
-		    'has_archive'           => true,
-		    'rewrite'               => true,
+            'menu_icon'             => 'dashicons-book-alt',
+            'hierarchical'          => false,
+            'supports'              => ['title', 'editor', 'custom-fields'],
+            'taxonomies'            => [],
+            'has_archive'           => true,
+            'rewrite'               => true,
             'query_var'             => true,
             'register_meta_box_cb'  => [__CLASS__, 'add_cpf_metaboxes'],
-	    ] );
+        ] );
     }
 
+    /**
+     * This method add metabox for cpf
+     *
+     * @return void
+     */
     public static function add_cpf_metaboxes(){
         $screens = array(self::$cpf_slug);
         add_meta_box(
@@ -74,15 +83,28 @@ class WP_REST_CPF_META_FIELD {
     }
 
    
+
+    /**
+     * display cpt meta fields
+     *
+     * @param string/int $post
+     * @param array $meta
+     * @return void
+    */
     public static function display_cpf_fields($post, $meta) {
-	    global $post;
-	    $cpf_meta = get_post_meta( $post->ID, self::$cpf_meta, true );
+        global $post;
+        $cpf_meta = get_post_meta( $post->ID, self::$cpf_meta, true );
         echo '<label>'.self::$cpf_meta_label.'</label><input type="text" name="'.self::$cpf_meta.'" value="' . esc_textarea( $cpf_meta )  . '" class="widefat">';
     }    
 
 
 
-
+    /**
+     * This method save cpf meta fields
+     *
+     * @param string/int $post_id
+     * @return void
+    */
     public static function save_cpf_meta( $post_id ) {
         
         if ( wp_is_post_revision( $post_id ) ){
@@ -93,28 +115,33 @@ class WP_REST_CPF_META_FIELD {
             return $post_id;
         endif;
 
-	    if (  ! isset( $_POST[self::$cpf_meta] ) ):
+        if (  ! isset( $_POST[self::$cpf_meta] ) ):
             return $post_id;
         endif;
         
         $events_meta[self::$cpf_meta] = esc_textarea( $_POST[self::$cpf_meta] );
 
-	    foreach ( $events_meta as $key => $value ) :
+        foreach ( $events_meta as $key => $value ) :
 
-		    if ( get_post_meta( $post_id, $key, false ) ) {
-			    update_post_meta( $post_id, $key, $value );
-		    } else {
-			    add_post_meta( $post_id, $key, $value);
-		    }
+            if ( get_post_meta( $post_id, $key, false ) ) {
+                update_post_meta( $post_id, $key, $value );
+            } else {
+                add_post_meta( $post_id, $key, $value);
+            }
 
-		    if ( ! $value ) {
-			    delete_post_meta( $post_id, $key );
-		    }
+            if ( ! $value ) {
+                delete_post_meta( $post_id, $key );
+            }
 
-	    endforeach;
+        endforeach;
 
     }
 
+    /**
+     * This method register the api rest field
+     *     
+     * @return void
+    */
     public static function add_cpf_data() {
         register_rest_field(self::$cpf_slug,
             self::$cpf_meta,
@@ -125,26 +152,19 @@ class WP_REST_CPF_META_FIELD {
         );
     }
      
+    /**
+     * This method gets the fields for the method add_cpf_data
+     *     
+     * @return void
+    */
     public static function cpf_get_field($object = '', $field_name = '', $request = array()) {
       return get_post_meta($object['id'], self::$cpf_meta, true);
-  
 
-
-    }
-
-
-    // public static function add_cpf_data() {   register_rest_field(self::$cpf_slug,
-    //         self::$cpf_meta,
-    //         array(
-    //             'get_callback' => [__CLASS__, 'cpf_get_field'],
-    //             'schema' => null,
-    //         )
-    //     );
-    // }
-  
+    } 
 
 
 }
 
 WP_REST_CPF_META_FIELD::init();
+
 ?>
